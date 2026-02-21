d2c025a2521b0454fa68e75c65e43800dd256f65
sathugvs428 <Santhugvs428@gmail.com>
2026-02-21 16:11:38 +0530

    Adding day4_python

---
+++ Python/Day4/Assignments/Program1.py
@@ -0,0 +1,33 @@
+'''Assignment
+----------
+
+program.py 
+----------
+
+input staements 
+print statements
+
+output
+------
+
+Enter personame 
+srinivas 
+
+Enter person location 
+Hyderbad
+
+Enter person gender 
+male 
+
+
+Person Name: srinivas 
+Person Location : Hyderbad
+Person Gender   : male  
+'''
+
+Person_name=input('Enter Person name\n')
+Person_location=input('Enter person Loctaion\n')
+person_gender=input("Enter Person Gender\n")
+print('Person Name is ',Person_name)
+print("Person Location is ",Person_location)
+print('Person Gender is ',person_gender)
---
+++ Python/Day4/Assignments/Program2.py
@@ -0,0 +1,36 @@
+'''
+2) 
+
+program.py 
+---------
+
+input --> product details 
+print --> product details 
+
+
+output
+------
+Enter product id 
+P1 
+Enter product name 
+mobile 
+Enter product price 
+3000
+Enter product qty 
+5
+
+*** PRODUCT DETAILS*****
+
+Product id is P1 
+product name is mobile 
+product price is 3000
+product quantity is 5 
+'''
+Product_id=input('Enter Product_id\n')
+Product_name=input('Enter Product Name\n')
+Product_price=input('Enter Product price\n')
+Product_quantity=input('Enter Quantity\n')
+print('Product Id is ',Product_id)
+print('Product Name is ',Product_name)
+print('Product price is ',Product_price)
+print('Product Quantity is ',Product_quantity)
